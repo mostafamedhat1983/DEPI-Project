eb6481f692c97f68eaee8b2fa93702664fa5f559
Marwan Sabry <70548281+MarwanSabry@users.noreply.github.com>
2025-03-19 14:40:20 +0200

    Delete Terraform/modules/vpc directory
    Replaced with network directory

--- Terraform/modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-  description = "ID of the VPC"
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
-  description = "ID of the public subnet"
-}
