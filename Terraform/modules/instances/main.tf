5a969e5a0e284503ce48bea75a4c9994434ff2be
Marwan Sabry <70548281+MarwanSabry@users.noreply.github.com>
2025-03-19 22:44:43 +0200

    Update Instances main
    This update for removing the unneeded code for ssh access in main instances file

--- Terraform/modules/instances/main.tf
+++ Terraform/modules/instances/main.tf
@@ -8,32 +8,5 @@ resource "aws_instance" "main" {
   tags = {
     Name = each.key
   }
-
   key_name = var.key_pair.key_name
-  provisioner "file" {
-    content     = var.key_pair.public_key
-    destination = "/tmp/mykey.pub"
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file(var.key_pair.private_key_path)
-      host        = self.public_ip
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo mkdir -p /home/ubuntu/.ssh",
-      "sudo cat /tmp/mykey.pub >> /home/ubuntu/.ssh/authorized_keys",
-      "sudo chmod 0600 /home/ubuntu/.ssh/authorized_keys",
-      "sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys",
-      "rm /tmp/mykey.pub"
-    ]
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = var.key_pair.private_key_pem
-      host        = self.public_ip
-    }
-  }
 }
